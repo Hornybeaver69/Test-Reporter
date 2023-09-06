a319c9d1c2c0a60ab16719f1122b9021b79dad2d
aleksandarpesic1@outlook.com <aleksandarpesic1@outlook.com>
2023-09-06 20:48:55 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,12 +10,13 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    # Clear the existing data in 'Tabela1'
+    app_tables.tabela1.delete_all_rows()
     # Any code you write here will run before the form opens.
     self.repeating_panel_1.items = app_tables.tabela1.search()
     # Set up event handler for file upload
     self.file_loader_1.set_event_handler('change', self.file_loaded)
-    # Update the Data Grid to show the new data
-    self.repeating_panel_1.items = app_tables.tabela1.search()
+
 
   def file_loaded(self, file, **event_args):
     # Read the uploaded JSON file
@@ -24,8 +25,10 @@ class Form1(Form1Template):
     # Clear the existing data in 'Tabela1'
     app_tables.tabela1.delete_all_rows()
 
+    # JSON object example
+    # {"test-case":"glupi test 6", "status":true, "error-type":null}
+    # In JSON report file, each line is a new JSON object.
     data = []
-    #file_path = file.
     lines = json_content.split('\n')
     for json_object_line in lines:
       try:
@@ -35,22 +38,17 @@ class Form1(Form1Template):
         return
       else:
          data.append(json_object)
-
-    # # Parse the JSON content
-    # try:
-    #   data = json.loads(json_content)
-    #   print(data)
-    # except ValueError as e:
-    #   # Handle JSON parsing error if necessary
-    #   #self.label_error.text = f"Error parsing JSON: {str(e)}"
-    #   print("Json parsing error")
-    #   return
       
     # Insert new data from the JSON into 'Tabela1'
     for item in data:
-      print(item.get("test-case"))
       app_tables.tabela1.add_row(
         test_case=item.get("test-case"),
         status=item.get("status"),
         error_type=item.get("error-type")
         )
+
+    # Update the Data Grid to show the new data
+    self.repeating_panel_1.items = app_tables.tabela1.search()
+    self.repeating_panel_1.items = app_tables.tabela1.search()
+    self.file_loader_1.clear()
+
